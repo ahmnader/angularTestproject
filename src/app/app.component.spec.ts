6e17a4bfc354c2bee75e0d6b73e70e3a7f40ce3c
anbadawy <anbadawy@LPALXY0605>
2023-06-25 15:24:12 +0300

    add unit test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toEqual('Hello angular Jenkins Sunday');
+    expect(compiled.querySelector('h1').textContent).toEqual('Hello angular Jenkins Sunday 2');
   }));
 
   it('should render title in a h1 tag', async(() => {
